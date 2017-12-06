816698d76b26b85e57b35afa3bbb287e6809dcec
Ali Sarfaraz <ali.sarfaraz@peakadventuretravel.com>
2017-12-07 08:56:35 +1100

    Added docker and travis directories to clean folders

---
+++ .travis/deploy/develop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+- docker tag ali-sarfaraz/docker-test-symfony:develop 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-symfony:develop
+- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-symfony:develop
+- docker tag ali-sarfaraz/docker-test-php:develop 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-php:develop
+- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-php:develop
+- docker tag ali-sarfaraz/docker-test-nginx:develop 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-nginx:develop
+- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-nginx:develop
---
+++ .travis/deploy/master.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+- docker tag ali-sarfaraz/docker-test-symfony:latest 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-symfony:latest
+- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-symfony:latest
+- docker tag ali-sarfaraz/docker-test-php:latest 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-php:latest
+- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-php:latest
+- docker tag ali-sarfaraz/docker-test-nginx:latest 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-nginx:latest
+- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-nginx:latest
