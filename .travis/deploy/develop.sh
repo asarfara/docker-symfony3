1bae22056f6a0e811adddf655f418e708ade52ee
Ali Sarfaraz <ali.sarfaraz@peakadventuretravel.com>
2017-12-09 14:37:40 +1100

    Testing deploy to aws

--- .travis/deploy/develop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-- docker tag ali-sarfaraz/docker-test-php:develop 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-php:develop
-- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-php:develop
-- docker tag ali-sarfaraz/docker-test-nginx:develop 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-nginx:develop
-- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-nginx:develop
--- .travis/deploy/master.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-- docker tag ali-sarfaraz/docker-test-php:latest 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-php:latest
-- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-php:latest
-- docker tag ali-sarfaraz/docker-test-nginx:latest 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-nginx:latest
-- docker push 550760950390.dkr.ecr.ap-southeast-2.amazonaws.com/ali-sarfaraz/docker-test-nginx:latest
