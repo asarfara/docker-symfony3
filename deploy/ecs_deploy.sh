b5c8980ccddad21d2346f93f48912c418f20de17
Ali Sarfaraz <ali.sarfaraz@peakadventuretravel.com>
2017-12-13 15:52:21 +1100

    Testing continous deployment using travis to deploy to aws

--- deploy/ecs_deploy.sh
+++ deploy/ecs_deploy.sh
@@ -4,7 +4,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "develop" ]; then
     echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
-    $TRAVIS_BUILD_DIR/deploy/ecs-deploy.sh -c $TASK_DEFINITION -n $SERVICE -i $REMOTE_IMAGE_URL:$TRAVIS_BRANCH
+    $TRAVIS_BUILD_DIR/deploy/ecs-deploy.sh -c $TASK_DEFINITION -n $SERVICE -i $REMOTE_IMAGE_URL:latest
   else
     echo "Skipping deploy because it's not an allowed branch"
   fi
