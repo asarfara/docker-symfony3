0696dee311d39b2705da03e04e2e6e6d514e7a23
Ali Sarfaraz <ali.sarfaraz@peakadventuretravel.com>
2017-12-13 14:56:40 +1100

    Testing continous deployment using travis to deploy to aws

--- deploy/docker_push.sh
+++ deploy/docker_push.sh
@@ -11,14 +11,14 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
 
     # Build and push
-    docker build -t $PHP_IMAGE_NAME -f .docker/php/
+    docker build -t $PHP_IMAGE_NAME -f .docker/php/Dockerfile
     echo "Pushing $PHP_IMAGE_NAME:latest"
     docker tag $PHP_IMAGE_NAME:latest "$REMOTE_IMAGE_URL/$PHP_IMAGE_NAME:latest"
     docker push "$REMOTE_IMAGE_URL/$PHP_IMAGE_NAME:latest"
     echo "Pushed $PHP_IMAGE_NAME:latest"
 
     # Build and push
-    docker build -t $WEB_IMAGE_NAME -f .docker/web/
+    docker build -t $WEB_IMAGE_NAME -f .docker/web/Dockerfile
     echo "Pushing $WEB_IMAGE_NAME:latest"
     docker tag $WEB_IMAGE_NAME:latest "$REMOTE_IMAGE_URL/$WEB_IMAGE_NAME:latest"
     docker push "$REMOTE_IMAGE_URL/$WEB_IMAGE_NAME:latest"
