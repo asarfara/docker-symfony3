ac9c55e768c880122d8bdbfb62921ad3f3ddf810
Ali Sarfaraz <ali.sarfaraz@peakadventuretravel.com>
2017-12-18 15:10:13 +1100

    Travis integration to ECS

--- deploy/ecs_deploy.sh
+++ deploy/ecs_deploy.sh
@@ -4,7 +4,6 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "develop" ]; then
     echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
-    $TRAVIS_BUILD_DIR/deploy/ecs-deploy.sh -c docker-test-symfony -n $SERVICE -i $REMOTE_IMAGE_URL/$PHP_IMAGE_NAME:latest
     $TRAVIS_BUILD_DIR/deploy/ecs-deploy.sh -c docker-test-symfony -n $SERVICE -i $REMOTE_IMAGE_URL/$WEB_IMAGE_NAME:latest
   else
     echo "Skipping deploy because it's not an allowed branch"
