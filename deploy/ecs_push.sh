0a826118b06aa35fd8cbcb8dc556e738601b279e
Ali Sarfaraz <ali.sarfaraz@peakadventuretravel.com>
2017-12-19 16:41:16 +1100

    Travis steps to show pipeline

--- deploy/ecs_push.sh
+++ deploy/ecs_push.sh
@@ -4,7 +4,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "develop" ]; then
     echo "Deploying $TRAVIS_BRANCH on $CLUSTER"
-    $TRAVIS_BUILD_DIR/deploy/ecs-deploy.sh -c $CLUSTER -n $SERVICE -i $REMOTE_IMAGE_URL/$WEB_IMAGE_NAME:latest --enable-rollback
+    $TRAVIS_BUILD_DIR/deploy/ecs-deploy.sh -c $CLUSTER -n $SERVICE -i $REMOTE_IMAGE_URL/$WEB_IMAGE_NAME:latest
   else
     echo "Skipping deploy because it's not an allowed branch"
   fi
